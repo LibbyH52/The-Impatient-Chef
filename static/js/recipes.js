b5f3c45a38a98d267520080530aef9f505d1ff00
Libby Henry <libbyh52@gmail.com>
2021-01-30 21:07:20 +0000

    Increased number of recipes returned from 6 to 9

--- static/js/recipes.js
+++ static/js/recipes.js
@@ -1,6 +1,8 @@
-const getRecipes = async (recipeName) => {
+const apiKey = "7dccf009d1d3499ab7f9fa0b154fdc3d";
+
+const getRecipes = async (recipeName) => { 
     const url = "https://api.spoonacular.com/recipes/complexSearch";
-    const query = `?apiKey=${apiKey}&query=${recipeName}&number=6`;
+    const query = `?apiKey=${apiKey}&query=${recipeName}&number=9`;
 
     const response = await fetch(url+query);
     const data = await response.json();
