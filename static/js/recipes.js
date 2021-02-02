deca7e837de7c5708056f5e2fc9431f2735a6fc8
Libby Henry <libbyh52@gmail.com>
2021-02-02 17:23:10 +0000

    Functionality for adding ingredients, remained ingredients to ingredientSt in relevant files

--- static/js/recipes.js
+++ static/js/recipes.js
@@ -1,9 +1,9 @@
 const apiKey = "7dccf009d1d3499ab7f9fa0b154fdc3d";
 
 
-const getRecipes = async (recipeName, diet, meal, cuisine, ingredients) => {
+const getRecipes = async (recipeName, diet, meal, cuisine, ingredientStr) => {
     const url = "https://api.spoonacular.com/recipes/complexSearch";
-    const query = `?apiKey=${apiKey}&query=${recipeName}&diet=${diet}&type=${meal}&cuisine=${cuisine}&includeIngredients=${ingredients}&number=9`;
+    const query = `?apiKey=${apiKey}&query=${recipeName}&diet=${diet}&type=${meal}&cuisine=${cuisine}&includeIngredients=${ingredientStr}&number=9`;
 
     const response = await fetch(url+query);
     const data = await response.json();
--- static/js/script.js
+++ static/js/script.js
@@ -8,10 +8,14 @@ const recipeContainer = document.querySelector('.recipe-container');
 const dietType = document.querySelectorAll('input[name="diet-type"]');
 const mealType = document.querySelectorAll('input[name="meal-type"]');
 const cuisineType = document.querySelectorAll('input[name="cuisine"]');
+const allergenList = document.querySelectorAll('input[class="allergens"]');
+const ingredient = document.querySelector('input[name="ingredient"]');
 
-const addBtn = document.querySelector('#add-btn');
+const addBtn = document.querySelector('.add-btn');
 const removeBtn = document.querySelector('#remove-btn');
 
+const ingredientList = document.querySelector('#ingredient-list');
+let ingredients = [];
 let id = 0;
 
 
@@ -38,14 +42,37 @@ const allRecipes = async (recipeName, diet, meal, cuisine, ingredients) => {
     return { recipeList: recipeList }
 }
 
+
+addBtn.addEventListener("click", () => {
+    if(ingredient.value !== ''){
+        ingredients.push(ingredient.value.trim());
+        let badge = document.createElement('span');
+        ingredientList.appendChild(badge);
+        badge.classList.add('ingredient-badge');
+        badge.textContent = ingredient.value;
+        ingredient.value = '';
+    }
+});
+
 filterForm.addEventListener("submit", e => {
     e.preventDefault();
     let diet = '';
     let meal = '';
-
+    let cuisine = '';
+    let ingredientStr = '';
+    ingredientList.innerHTML = '';
+    
     //get the recipe name from the user
     const recipeName = document.querySelector('#recipe-search').value.trim();
-    let ingredients = document.querySelector('.ingredient-search').value.trim().split(' ').join(',');
+    if(ingredients.length > 1) {
+        ingredientStr = ingredients.join(',');
+    }
+
+    for(let i=0; i<allergenList.length; i++) {
+        if(allergenList[i].checked) {
+            console.log(allergenList[i].value);
+        }
+    }
 
     for(let i=0; i<dietType.length; i++) {
         if(dietType[i].checked) {
@@ -67,7 +94,7 @@ filterForm.addEventListener("submit", e => {
 
     filterForm.reset();
 
-    allRecipes(recipeName, diet, meal, cuisine, ingredients)
+    allRecipes(recipeName, diet, meal, cuisine, ingredientStr)
         .then(data => displayRecipes(data))
         .catch(err => console.log(err));
 });
